7630d30afa70aa646a136b97d0c830af2ef5ad0e
Tim Castillo <tim@elementl.com>
2023-06-13 11:54:25 -0400

    breaking up the assets to reduce redundant I/O

--- dagster_university/assets.py
+++ dagster_university/assets.py
@@ -2,50 +2,51 @@ from dagster import asset, get_dagster_logger
 from dagster_duckdb import DuckDBResource
 
 import requests
-import tempfile
 
 @asset
-def taxi_zones(database: DuckDBResource):
+def taxi_zones_file():
     raw_taxi_zones = requests.get(
         "https://data.cityofnewyork.us/api/geospatial/d3c5-ddgc?method=export&format=GeoJSON"
-    ).content
+    )
 
-    with tempfile.NamedTemporaryFile(suffix=".geojson") as tmpfile:
-        with open(tmpfile.name, "wb") as f:
-            f.write(raw_taxi_zones)
-
-        query = f"""
-            create or replace table zones as (
-            select
-                zone,
-                location_id,
-                borough,
-                st_geomfromwkb(wkb_geometry) as geom 
-            from st_read('{tmpfile.name}')
-            );
-        """
-
-        with database.get_connection() as conn:
-            conn.install_extension("spatial")
-            conn.load_extension("spatial")
-            conn.execute(query)
+    with open("taxi_zones.geojson", "wb") as f:
+        f.write(raw_taxi_zones.content)
 
 @asset
-def taxi_trips(database: DuckDBResource):
+def taxi_zones(taxi_zones_file, database: DuckDBResource):
+    query = f"""
+        create or replace table zones as (
+        select
+            zone,
+            location_id,
+            borough,
+            st_geomfromwkb(wkb_geometry) as geom 
+        from st_read('taxi_zones.geojson')
+        );
+    """
+
+    with database.get_connection() as conn:
+        conn.install_extension("spatial")
+        conn.load_extension("spatial")
+        conn.execute(query)
+
+@asset
+def taxi_trips_file():
     raw_trips = requests.get(
         "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2023-01.parquet"
-    ).content
+    )
 
-    with tempfile.NamedTemporaryFile(suffix=".parquet") as tmpfile:
-        with open(tmpfile.name, "wb") as f:
-            f.write(raw_trips)
-        
-        query = f"""
-            create or replace table raw_trips as (
-                select *
-                from '{tmpfile.name}'
-            );
-        """
+    with open("yellow_tripdata_2023-01.parquet", "wb") as f:
+        f.write(raw_trips.content)
 
-        with database.get_connection() as conn:
-            conn.execute(query)
+@asset
+def taxi_trips(taxi_trips_file, database: DuckDBResource):
+    query = f"""
+        create or replace table raw_trips as (
+            select *
+            from 'yellow_tripdata_2023-01.parquet'
+        );
+    """
+
+    with database.get_connection() as conn:
+        conn.execute(query)
