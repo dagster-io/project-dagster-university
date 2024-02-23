bf164ee5a8dc383d9b9f8d01bf661810c5033831
Erin Cochran <erin.k.cochran@gmail.com>
2024-02-23 10:12:29 -0500

    Remove duplicate constants
    This PR removes duplicate constants from `assets/constants.py`. Opting to go for the `os.getpath` option to be consistent with how other variables are constructed.

--- dagster_university/assets/constants.py
+++ dagster_university/assets/constants.py
@@ -1,8 +1,5 @@
 import os
 
-TAXI_ZONES_FILE_PATH = "data/raw/taxi_zones.csv"
-TAXI_TRIPS_TEMPLATE_FILE_PATH = "data/raw/taxi_trips_{}.parquet"
-
 TAXI_ZONES_FILE_PATH = os.path.join("data", "raw", "taxi_zones.csv")
 TAXI_TRIPS_TEMPLATE_FILE_PATH = os.path.join("data", "raw", "taxi_trips_{}.parquet")
 
