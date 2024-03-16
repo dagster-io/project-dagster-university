4c7981eb8e8ceb1aacbc1b93e6a186881cdbd173
erikjamesmason <ejmdpg@gmail.com>
2024-03-16 08:54:14 -0700

    dbt changes

--- dagster_university/assets/dbt.py
+++ dagster_university/assets/dbt.py
@@ -1,1 +1,14 @@
 from dagster import AssetExecutionContext
+from dagster_dbt import dbt_assets, DbtCliResource
+
+import os
+
+from .constants import DBT_DIRECTORY
+
+dbt_manifest_path = os.path.join(DBT_DIRECTORY, "target", "manifest.json")
+
+@dbt_assets(
+    manifest=dbt_manifest_path,
+)
+def dbt_analytics(context: AssetExecutionContext, dbt: DbtCliResource):
+    yield from dbt.cli(["run"], context=context).stream()
