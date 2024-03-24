c54e4a5b127ea1e0876751106ef8960a55e4df2a
erikjamesmason <ejmdpg@gmail.com>
2024-03-24 08:48:42 -0700

    little progress: diligent but slow mfers

--- dagster_university/assets/dbt.py
+++ dagster_university/assets/dbt.py
@@ -1,14 +1,37 @@
-from dagster import AssetExecutionContext
-from dagster_dbt import dbt_assets, DbtCliResource
-
 import os
+from dagster import AssetExecutionContext, AssetKey
+from dagster_dbt import dbt_assets, DbtCliResource, DagsterDbtTranslator
 
 from .constants import DBT_DIRECTORY
+from ..resources import dbt_resource
 
-dbt_manifest_path = os.path.join(DBT_DIRECTORY, "target", "manifest.json")
+
+class CustomizeddDagsterDbtTranslator(DagsterDbtTranslator):
+    @classmethod
+    def get_asset_key(cls, dbt_resource_props):
+        resource_type = dbt_resource_props["resource_type"]
+        name = dbt_resource_props["name"]
+
+        if resource_type == "source":
+            return AssetKey(f"taxi_{name}")
+        else:
+            return super().get_asset_key(dbt_resource_props)
+
+
+dbt_resource.cli(["--quiet", "parse"]).wait()
+
+if os.getenv("DAGSTER_DBT_PARSE_PROJECT_ON_LOAD"):
+    dbt_manifest_path = (
+        dbt_resource.cli(["--quiet", "parse"])
+        .wait()
+        .target_path.joinpath("manifest.json")
+    )
+else:
+    dbt_manifest_path = os.path.join(DBT_DIRECTORY, "target", "manifest.json")
+
 
 @dbt_assets(
-    manifest=dbt_manifest_path,
+    manifest=dbt_manifest_path, dagster_dbt_translator=CustomizeddDagsterDbtTranslator()
 )
 def dbt_analytics(context: AssetExecutionContext, dbt: DbtCliResource):
-    yield from dbt.cli(["run"], context=context).stream()
+    yield from dbt.cli(["build"], context=context).stream()
