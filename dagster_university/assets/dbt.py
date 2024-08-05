844427b5db975c32307568f488baa8584782f002
Maxime Armstrong <maxime@retain.ai>
2024-08-05 13:55:00 -0400

    Update code to use DbtProject

--- dagster_university/assets/dbt.py
+++ dagster_university/assets/dbt.py
@@ -1,53 +1,43 @@
-from dagster import AssetKey, AssetExecutionContext
-from dagster_dbt import dbt_assets, DbtCliResource, DagsterDbtTranslator
+import json
+import os
 from pathlib import Path
 
-import os
-import json
+from dagster import AssetExecutionContext, AssetKey
+from dagster_dbt import DagsterDbtTranslator, DbtCliResource, DbtProject, dbt_assets
 
-from .constants import DBT_DIRECTORY
-from ..resources import dbt_resource
 from ..partitions import daily_partition
 
-class CustomizedDagsterDbtTranslator(DagsterDbtTranslator):
+INCREMENTAL_SELECTOR = "config.materialized:incremental"
 
-    @classmethod
-    def get_asset_key(cls, dbt_resource_props):
+dbt_project = DbtProject(
+    project_dir=Path(__file__).joinpath("..", "..", "..", "analytics").resolve(),
+    target=os.getenv("DBT_TARGET"),
+)
+dbt_project.prepare_if_dev()
+
+
+class CustomizedDagsterDbtTranslator(DagsterDbtTranslator):
+    def get_asset_key(self, dbt_resource_props):
         type = dbt_resource_props["resource_type"]
         name = dbt_resource_props["name"]
         if type == "source":
             return AssetKey(f"taxi_{name}")
         else:
             return super().get_asset_key(dbt_resource_props)
-        
-    @classmethod
-    def get_metadata(cls, dbt_node_info):
+
+    def get_metadata(self, dbt_node_info):
         return {
             "columns": dbt_node_info["columns"],
             "sources": dbt_node_info["sources"],
             "description": dbt_node_info["description"],
         }
 
-    @classmethod
-    def get_group_name(cls, dbt_resource_props):
+    def get_group_name(self, dbt_resource_props):
         return dbt_resource_props["fqn"][1]
-        
-if os.getenv("DAGSTER_DBT_PARSE_PROJECT_ON_LOAD"):
-    dbt_manifest_path = (
-        dbt_resource.cli(
-            ["--quiet", "parse"],
-            target_path=Path("target")
-        )
-        .wait()
-        .target_path.joinpath("manifest.json")
-    )
-else:
-    dbt_manifest_path = os.path.join(DBT_DIRECTORY, "target", "manifest.json")
 
-INCREMENTAL_SELECTOR = "config.materialized:incremental"
 
 @dbt_assets(
-    manifest=dbt_manifest_path,
+    manifest=dbt_project.manifest_path,
     dagster_dbt_translator=CustomizedDagsterDbtTranslator(),
     exclude=INCREMENTAL_SELECTOR,
 )
@@ -62,7 +52,7 @@ def dbt_analytics(context: AssetExecutionContext, dbt: DbtCliResource):
 
 
 @dbt_assets(
-    manifest=dbt_manifest_path,
+    manifest=dbt_project.manifest_path,
     dagster_dbt_translator=CustomizedDagsterDbtTranslator(),
     select=INCREMENTAL_SELECTOR,
     partitions_def=daily_partition,
