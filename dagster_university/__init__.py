662cb0abdba062cd72c0a0a0114639b57db545ac
Dennis Hume <dennis@dagsterlabs.com>
2025-01-08 15:28:19 -0600

    Include int steps

--- dagster_university/__init__.py
+++ dagster_university/__init__.py
@@ -1,6 +1,6 @@
 from dagster import Definitions, load_assets_from_modules
 
-from .assets import trips, metrics, requests
+from .assets import trips, metrics, requests, _trips
 from .resources import database_resource
 from .jobs import trip_update_job, weekly_update_job, adhoc_request_job
 from .schedules import trip_update_schedule, weekly_update_schedule
@@ -20,6 +20,8 @@ all_jobs = [trip_update_job, weekly_update_job, adhoc_request_job]
 all_schedules = [trip_update_schedule, weekly_update_schedule]
 all_sensors = [adhoc_request_sensor]
 
+# Only use the definition that applies to the section you are running
+# This is mostly when updating screenshots
 defs = Definitions(
     assets=trip_assets + metric_assets + requests_assets,
     resources={
@@ -28,4 +30,38 @@ defs = Definitions(
     jobs=all_jobs,
     schedules=all_schedules,
     sensors=all_sensors,
+    metadata={
+        "lessons": ["1","2","5","7","8","9"]
+    },
 )
+
+# defs = Definitions(
+#     assets=[_trips.taxi_trips_file_lesson_3],
+#     metadata={
+#         "lessons": ["3"]
+#     },
+# )
+
+# defs = Definitions(
+#     assets=[trips.taxi_trips_file, trips.taxi_zones_file],
+#     metadata={
+#         "lessons": ["4a"]
+#     },
+# )
+
+# defs = Definitions(
+#     assets=[trips.taxi_trips_file, trips.taxi_zones_file, trips.taxi_trips],
+#     metadata={
+#         "lessons": ["4b"]
+#     },
+# )
+
+# defs = Definitions(
+#     assets=[trips.taxi_trips, trips.taxi_zones, metrics.trips_by_week, metrics.manhattan_stats],
+#     resources={
+#         "database": database_resource,
+#     },
+#     metadata={
+#         "lessons": ["6"]
+#     },
+# )
---
+++ dagster_university/assets/_trips.py
@@ -0,0 +1,21 @@
+""""
+    Iterations of assets for earlier lessons
+"""
+import requests
+from . import constants
+from dagster import asset
+
+@asset(
+    name="taxi_trips_file",
+)
+def taxi_trips_file_lesson_3() -> None:
+    """
+      The raw parquet files for the taxi trips dataset. Sourced from the NYC Open Data portal.
+    """
+    month_to_fetch = '2023-03'
+    raw_trips = requests.get(
+        f"https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{month_to_fetch}.parquet"
+    )
+
+    with open(constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch), "wb") as output_file:
+        output_file.write(raw_trips.content)
