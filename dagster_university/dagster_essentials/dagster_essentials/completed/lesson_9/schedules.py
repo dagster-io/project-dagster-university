2849e8629655ee82c0954cfcaa3b6b546d6ddda8
Dennis Hume <dennis@dagsterlabs.com>
2025-03-24 09:27:47 -0500

    Fix relative paths Essentials

--- dagster_university/dagster_essentials/dagster_essentials/completed/lesson_9/schedules.py
+++ dagster_university/dagster_essentials/dagster_essentials/completed/lesson_9/schedules.py
@@ -1,6 +1,6 @@
 import dagster as dg
 
-from ..jobs import trip_update_job, weekly_update_job
+from .jobs import trip_update_job, weekly_update_job
 
 trip_update_schedule = dg.ScheduleDefinition(
     job=trip_update_job,
