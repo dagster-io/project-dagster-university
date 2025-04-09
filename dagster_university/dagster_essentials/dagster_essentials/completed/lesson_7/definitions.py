6318519d6fe74668f9b6ceb4c111553de6182655
Dennis Hume <dennis@dagsterlabs.com>
2025-04-09 11:50:00 -0500

    Check

--- dagster_university/dagster_essentials/dagster_essentials/completed/lesson_7/definitions.py
+++ dagster_university/dagster_essentials/dagster_essentials/completed/lesson_7/definitions.py
@@ -1,5 +1,13 @@
 import dagster as dg
 
 import dagster_essentials.completed.lesson_7.defs as defs
+from dagster_essentials.completed.lesson_7.defs.resources import database_resource
 
-defs = dg.components.load_defs(defs)
+defs = dg.Definitions.merge(
+    dg.Definitions(
+        resources={
+            "database": database_resource,
+        },
+    ),
+    dg.components.load_defs(defs),
+)
