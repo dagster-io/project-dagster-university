3ca862d06e36149e098e5415289908f81fd23c50
Eddy Z <74501266+eyzyly@users.noreply.github.com>
2025-06-22 22:27:43 +0000

    define asset

---
+++ dagster_university/dagster_essentials/dagster_essentials/assets/trips.py
@@ -0,0 +1,17 @@
+import requests
+from dagster_essentials.assets import constants
+
+import dagster as dg
+
+@dg.asset
+def taxi_trips_file() -> None:
+    """
+      The raw parquet files for the taxi trips dataset. Sourced from the NYC Open Data portal.
+    """
+    month_to_fetch = '2023-03'
+    raw_trips = requests.get(
+        f"https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{month_to_fetch}.parquet"
+    )
+
+    with open(constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch), "wb") as output_file:
+        output_file.write(raw_trips.content)
