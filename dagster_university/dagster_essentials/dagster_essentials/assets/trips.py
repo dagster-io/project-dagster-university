791827bcd1bcbb8f9998cc74f00382c85ea7a61a
Eddy Z <74501266+eyzyly@users.noreply.github.com>
2025-06-22 22:41:09 +0000

    add taxi zones asset

--- dagster_university/dagster_essentials/dagster_essentials/assets/trips.py
+++ dagster_university/dagster_essentials/dagster_essentials/assets/trips.py
@@ -15,3 +15,15 @@ def taxi_trips_file() -> None:
 
     with open(constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch), "wb") as output_file:
         output_file.write(raw_trips.content)
+
+@dg.asset
+def taxi_zones_file() -> None:
+    """
+      The raw csv file for the taxi trips dataset containing taxi zones. Sourced from the NYC Open Data portal.
+    """
+    raw_taxi_zones = requests.get(
+        f"https://community-engineering-artifacts.s3.us-west-2.amazonaws.com/dagster-university/data/taxi_zones.csv"
+    )
+
+    with open(constants.TAXI_ZONES_FILE_PATH, "wb") as output_file:
+        output_file.write(raw_taxi_zones.content)
