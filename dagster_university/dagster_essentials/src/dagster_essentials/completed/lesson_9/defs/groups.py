317d2323182b78160c815375d32937362e6bb34d
Dennis Hume <dennis@dagsterlabs.com>
2025-08-07 08:03:34 -0500

    Remove sub module grouping

--- dagster_university/dagster_essentials/src/dagster_essentials/completed/lesson_9/defs/groups.py
+++
@@ -1,8 +0,0 @@
-import dagster as dg
-
-from dagster_essentials.completed.lesson_9.defs.assets import requests
-
-request_assets = dg.load_assets_from_modules(
-    modules=[requests],
-    group_name="requests",
-)
