8fd0105ca8aeb3ae09a207d7095cce1cc648db0b
Dennis Hume <dehume@users.noreply.github.com>
2025-09-04 14:40:43 -0500

    Include `DefsFolderComponent` content (#138)

--- dagster_university/dagster_essentials/tests/test_lesson_9.py
+++ dagster_university/dagster_essentials/tests/test_lesson_9.py
@@ -1,39 +1,28 @@
 from pathlib import Path
 
 import dagster as dg
+import pytest
 import yaml
 
 import dagster_essentials.completed.lesson_9.defs
-from dagster_essentials.completed.lesson_9.defs.assets import (
-    metrics,
-    requests,
-    trips,
-)
-from dagster_essentials.completed.lesson_9.defs.jobs import (
-    adhoc_request_job,
-    trip_update_job,
-    weekly_update_job,
-)
 from dagster_essentials.completed.lesson_9.defs.resources import database_resource
-from dagster_essentials.completed.lesson_9.defs.schedules import (
-    trip_update_schedule,
-    weekly_update_schedule,
-)
-from dagster_essentials.completed.lesson_9.defs.sensors import (
-    adhoc_request_sensor,
-)
 from tests.fixtures import drop_tax_trips_table  # noqa: F401
 
 
-def test_trips_partitioned_assets(drop_tax_trips_table):  # noqa: F811
+@pytest.fixture()
+def defs():
+    return dg.components.load_defs(dagster_essentials.completed.lesson_9.defs)
+
+
+def test_trips_partitioned_assets(drop_tax_trips_table, defs):  # noqa: F811
     assets = [
-        trips.taxi_trips_file,
-        trips.taxi_zones_file,
-        trips.taxi_trips,
-        trips.taxi_zones,
-        metrics.manhattan_stats,
-        metrics.manhattan_map,
-        requests.adhoc_request,
+        defs.get_assets_def(dg.AssetKey(["taxi_trips_file"])),
+        defs.get_assets_def(dg.AssetKey(["taxi_zones_file"])),
+        defs.get_assets_def(dg.AssetKey(["taxi_trips"])),
+        defs.get_assets_def(dg.AssetKey(["taxi_zones"])),
+        defs.get_assets_def(dg.AssetKey(["manhattan", "manhattan_stats"])),
+        defs.get_assets_def(dg.AssetKey(["manhattan_map"])),
+        defs.get_assets_def(dg.AssetKey(["adhoc_request"])),
     ]
     result = dg.materialize(
         assets=assets,
@@ -48,9 +37,9 @@ def test_trips_partitioned_assets(drop_tax_trips_table):  # noqa: F811
     assert result.success
 
 
-def test_trips_by_week_partitioned_assets():
+def test_trips_by_week_partitioned_assets(defs):
     assets = [
-        metrics.trips_by_week,
+        defs.get_assets_def(dg.AssetKey(["trips_by_week"])),
     ]
     result = dg.materialize(
         assets=assets,
@@ -62,27 +51,20 @@ def test_trips_by_week_partitioned_assets():
     assert result.success
 
 
-def test_jobs():
-    assert trip_update_job
-    assert weekly_update_job
-    assert adhoc_request_job
-
-    assert trip_update_job.name == "trip_update_job"
-    assert weekly_update_job.name == "weekly_update_job"
+def test_jobs(defs):
+    assert defs.get_job_def("trip_update_job")
+    assert defs.get_job_def("weekly_update_job")
+    assert defs.get_job_def("adhoc_request_job")
 
 
-def test_schedules():
-    assert trip_update_schedule.cron_schedule == "0 0 5 * *"
-    assert trip_update_schedule.job == trip_update_job
-    assert weekly_update_schedule.cron_schedule == "0 0 * * 1"
-    assert weekly_update_schedule.job == weekly_update_job
+def test_schedules(defs):
+    assert defs.get_schedule_def("trip_update_job_schedule")
+    assert defs.get_schedule_def("weekly_update_job_schedule")
 
 
-def test_sensors():
-    assert adhoc_request_sensor
+def test_sensors(defs):
+    assert defs.get_sensor_def("adhoc_request_sensor")
 
 
-def test_defs():
-    assert dg.Definitions.merge(
-        dg.components.load_defs(dagster_essentials.completed.lesson_9.defs)
-    )
+def test_defs(defs):
+    assert defs
