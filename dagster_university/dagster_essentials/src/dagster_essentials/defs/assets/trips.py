0d927952e4e31386f6b500ec35c594b3703ab7df
jsiryj <jsiryj@gmail.com>
2025-09-20 19:41:33 +0000

    Add DuckDB table materialisation

--- dagster_university/dagster_essentials/src/dagster_essentials/defs/assets/trips.py
+++ dagster_university/dagster_essentials/src/dagster_essentials/defs/assets/trips.py
@@ -1,6 +1,9 @@
 import requests
 from dagster_essentials.defs.assets import constants
+import duckdb
+import os
 import dagster as dg
+from dagster._utils.backoff import backoff
 
 @dg.asset
 def taxi_trips_file() -> None:
@@ -26,3 +29,37 @@ def taxi_zones_file() -> None:
 
     with open(constants.TAXI_ZONES_FILE_PATH, "wb") as output_file:
         output_file.write(raw_zones.content)
+
+@dg.asset(
+    deps=["taxi_trips_file"]
+)
+def taxi_trips() -> None:
+    """
+      The raw taxi trips dataset, loaded into a DuckDB database
+    """
+    query = """
+        create or replace table trips as (
+          select
+            VendorID as vendor_id,
+            PULocationID as pickup_zone_id,
+            DOLocationID as dropoff_zone_id,
+            RatecodeID as rate_code_id,
+            payment_type as payment_type,
+            tpep_dropoff_datetime as dropoff_datetime,
+            tpep_pickup_datetime as pickup_datetime,
+            trip_distance as trip_distance,
+            passenger_count as passenger_count,
+            total_amount as total_amount
+          from 'data/raw/taxi_trips_2023-03.parquet'
+        );
+    """
+
+    conn = backoff(
+        fn=duckdb.connect,
+        retry_on=(RuntimeError, duckdb.IOException),
+        kwargs={
+            "database": os.getenv("DUCKDB_DATABASE"),
+        },
+        max_retries=10,
+    )
+    conn.execute(query)
