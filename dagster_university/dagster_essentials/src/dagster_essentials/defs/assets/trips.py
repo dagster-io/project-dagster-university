a55e791680b3c2e04ed6655e42d6b3065bd1ac87
jsiryj <jsiryj@gmail.com>
2025-09-20 19:34:06 +0000

    Add zones asset

--- dagster_university/dagster_essentials/src/dagster_essentials/defs/assets/trips.py
+++ dagster_university/dagster_essentials/src/dagster_essentials/defs/assets/trips.py
@@ -14,3 +14,15 @@ def taxi_trips_file() -> None:
 
     with open(constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch), "wb") as output_file:
         output_file.write(raw_trips.content)
+
+@dg.asset
+def taxi_zones_file() -> None:
+    """
+      The raw parquet files for the taxi zones dataset. Sourced from the NYC Open Data portal.
+    """
+    raw_zones = requests.get(
+        f"https://community-engineering-artifacts.s3.us-west-2.amazonaws.com/dagster-university/data/taxi_zones.csv"
+    )
+
+    with open(constants.TAXI_ZONES_FILE_PATH, "wb") as output_file:
+        output_file.write(raw_zones.content)
