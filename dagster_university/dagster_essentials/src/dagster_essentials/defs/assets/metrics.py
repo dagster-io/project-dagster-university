13923dd5da00b0a57582210dcb746a294bffc032
jsiryj <jsiryj@gmail.com>
2025-09-20 19:57:27 +0000

    Add Manhattan stats asset logic

--- dagster_university/dagster_essentials/src/dagster_essentials/defs/assets/metrics.py
+++ dagster_university/dagster_essentials/src/dagster_essentials/defs/assets/metrics.py
@@ -8,5 +8,27 @@ import os
 
 from dagster_essentials.defs.assets import constants
 
-@dg.asset
-def metrics(context: dg.AssetExecutionContext) -> dg.MaterializeResult: ...
+@dg.asset(
+    deps=["taxi_trips", "taxi_zones"]
+)
+def manhattan_stats() -> None:
+    query = """
+        select
+            zones.zone,
+            zones.borough,
+            zones.geometry,
+            count(1) as num_trips,
+        from trips
+        left join zones on trips.pickup_zone_id = zones.zone_id
+        where borough = 'Manhattan' and geometry is not null
+        group by zone, borough, geometry
+    """
+
+    conn = duckdb.connect(os.getenv("DUCKDB_DATABASE"))
+    trips_by_zone = conn.execute(query).fetch_df()
+
+    trips_by_zone["geometry"] = gpd.GeoSeries.from_wkt(trips_by_zone["geometry"])
+    trips_by_zone = gpd.GeoDataFrame(trips_by_zone)
+
+    with open(constants.MANHATTAN_STATS_FILE_PATH, 'w') as output_file:
+        output_file.write(trips_by_zone.to_json())
