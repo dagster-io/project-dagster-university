c6ba03771253b26fd4d1d00b49a4a4c9a354ee07
jsiryj <jsiryj@gmail.com>
2025-09-21 15:59:25 +0000

    fix PATH var not existing

--- dagster_university/dagster_essentials/src/dagster_essentials/defs/assets/constants.py
+++ dagster_university/dagster_essentials/src/dagster_essentials/defs/assets/constants.py
@@ -2,6 +2,7 @@ TAXI_ZONES_FILE_PATH = "data/raw/taxi_zones.csv"
 TAXI_TRIPS_TEMPLATE_FILE_PATH = "data/raw/taxi_trips_{}.parquet"
 
 TRIPS_BY_AIRPORT_FILE_PATH = "data/outputs/trips_by_airport.csv"
+TRIPS_BY_WEEK_FILE_PATH = "data/outputs/trips_by_week.csv"
 TRIPS_BY_WEEK_MINE_FILE_PATH = "data/outputs/trips_by_week_mine.csv"
 MANHATTAN_STATS_FILE_PATH = "data/staging/manhattan_stats.geojson"
 MANHATTAN_MAP_FILE_PATH = "data/outputs/manhattan_map.png"
