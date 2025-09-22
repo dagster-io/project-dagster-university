5594e9d407f467a36245f9ca316e6ce138f0fc97
jsiryj <jsiryj@gmail.com>
2025-09-22 04:03:37 +0000

    over-wrote schedule name...

--- dagster_university/dagster_essentials/src/dagster_essentials/defs/schedules.py
+++ dagster_university/dagster_essentials/src/dagster_essentials/defs/schedules.py
@@ -6,7 +6,7 @@ trip_update_schedule = dg.ScheduleDefinition(
     cron_schedule="0 0 5 * *", # every 5th of the month at midnight
 )
 
-trip_update_schedule = dg.ScheduleDefinition(
+weekly_update_schedule = dg.ScheduleDefinition(
     job=weekly_update_job,
     cron_schedule="0 0 * * 1", # every Monday at midnight
 )
