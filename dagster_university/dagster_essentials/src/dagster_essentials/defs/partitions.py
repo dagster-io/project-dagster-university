78af04aa2d14ec090d7088062bd03c3db280a908
jsiryj <jsiryj@gmail.com>
2025-09-23 01:15:35 +0000

    Add missing MonthlyPartitionDefinition

--- dagster_university/dagster_essentials/src/dagster_essentials/defs/partitions.py
+++ dagster_university/dagster_essentials/src/dagster_essentials/defs/partitions.py
@@ -4,6 +4,11 @@ from dagster_essentials.defs.assets import constants
 start_date = constants.START_DATE
 end_date = constants.END_DATE
 
+monthly_partition = dg.MonthlyPartitionsDefinition(
+    start_date=start_date,
+    end_date=end_date
+)
+
 weekly_partition = dg.WeeklyPartitionsDefinition(
     start_date=start_date,
     end_date=end_date
