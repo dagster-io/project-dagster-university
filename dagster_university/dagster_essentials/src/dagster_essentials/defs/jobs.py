07deca532c963667a7dc92dc1ac10f2f2c198dae
jsiryj <jsiryj@gmail.com>
2025-09-23 01:24:06 +0000

    Add partition_def to trip_update_job job

--- dagster_university/dagster_essentials/src/dagster_essentials/defs/jobs.py
+++ dagster_university/dagster_essentials/src/dagster_essentials/defs/jobs.py
@@ -1,9 +1,11 @@
 import dagster as dg
+from dagster_essentials.defs.partitions import monthly_partition
 
 trips_by_week = dg.AssetSelection.assets("trips_by_week")
 
 trip_update_job = dg.define_asset_job(
     name="trip_update_job",
+    partitions_def=monthly_partition,
     selection=dg.AssetSelection.all() - trips_by_week
 )
 
