2054554dc2aaf950571f0d8322c7deb1bdaacf24
dnewman-phdata <dnewman@phdata.io>
2026-02-24 20:52:15 +0000

    foo

--- dagster_university/dagster_essentials/src/dagster_essentials/defs/assets/trips.py
+++ dagster_university/dagster_essentials/src/dagster_essentials/defs/assets/trips.py
@@ -13,9 +13,12 @@ def taxi_trips_file() -> None:
     with open(constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch), "wb") as output_file:
         output_file.write(raw_trips.content)
 
-@dg.asset 
+# src/dagster_essentials/defs/assets/trips.py
+@dg.asset
 def taxi_zones_file() -> None:
-    #get some more data 
+    """
+      The raw CSV file for the taxi zones dataset. Sourced from the NYC Open Data portal.
+    """
     raw_taxi_zones = requests.get(
         "https://community-engineering-artifacts.s3.us-west-2.amazonaws.com/dagster-university/data/taxi_zones.csv"
     )
